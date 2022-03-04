9c0bef3743e66573695743147302a45d0cc8a11d
p23ar <priyanka.arora@rudrainnovatives.com>
2022-03-04 13:31:47 +0530

    changes

--- public/partials/single-property-page.php
+++ public/partials/single-property-page.php
@@ -1,18 +1,41 @@
-<!--Loader div start-->
-<div class="loaderdiv text-center position-relative">
-	<div class="loader-content position-absolute">
-		<div class="running">
-	    <div class="outer">
-	        <div class="body">
-	            <div class="arm behind"></div>
-	            <div class="arm front"></div>
-	            <div class="leg behind"></div>
-	            <div class="leg front"></div>
-	        </div>
-	    </div>
-	</div>
-	<div class=" text-center mt-4">Fetching Data...</div>
-	</div>
+<!--Property info loader div start-->
+<div class="loaderdiv skeleton-div">
+    <div class="d-flex flex-wrap align-items-center loading-skeleton">
+        <div class="loading-skeleton w-100">
+            <img src="/wp-content/uploads/2022/02/loader-img.png" class="card-img-top" alt="...">
+        </div>
+    </div>
+    <div class="d-flex flex-wrap my-5 mw-1140 px-2 loading-skeleton">
+        <div class="loading-skeleton col-lg-60 px-3">
+            <p>Lorem Ipsum is simply dummy </p>
+            <div class="col-lg-80 mt-5 px-0">
+            <p>Lorem Ipsum is simply dummy </p>
+            </div>
+            <div class="col-lg-70 mt-5 px-0">
+            <p>Lorem Ipsum is simply dummy </p>
+            </div>
+            <div class="col-lg-100 px-0 mt-5"> 
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <div class="d-flex w-100 loader-info-div mt-5">
+                    <div class="col-lg-50">
+                        <p>Lorem Ipsum is simply dummy </p>
+                        <p>Lorem Ipsum is simply dummy </p>
+                        <p>Lorem Ipsum is simply dummy </p>
+                        <p>Lorem Ipsum is simply dummy </p>
+                    </div>
+                <div class="col-lg-50">
+                        <p>Lorem Ipsum is simply dummy </p>
+                        <p>Lorem Ipsum is simply dummy </p>
+                        <p>Lorem Ipsum is simply dummy </p>
+                        <p>Lorem Ipsum is simply dummy </p>
+                    </div></div>
+            </div>
+        </div>
+        <div class="loading-skeleton col-lg-40 px-3">
+            <img src="/wp-content/uploads/2022/02/loader-image2.png" class="card-img-top" alt="...">
+            <img src="/wp-content/uploads/2022/02/loader-image2.png" class="card-img-top mt-4" alt="...">
+        </div>
+    </div>
 </div>
 <!--Loader div end-->
 <!-- property banner starts here-->
